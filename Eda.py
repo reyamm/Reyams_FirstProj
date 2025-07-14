04275b63a7a2f77843debea44dbed86f5ddcad74
reyamm <148452483+reyamm@users.noreply.github.com>
2025-07-14 23:07:51 +0300

    Create Eda.py

---
+++ Eda.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data.csv")
+
+print(df.info())
+print(df.describe())
+
+df['Subject'].value_counts().plot(kind='bar', title="Resources per Subject")
+plt.xlabel("Subject")
+plt.ylabel("Count")
+plt.tight_layout()
+plt.show()
